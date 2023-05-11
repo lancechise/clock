b7c56fd93242b7a9b0c54913d85a8726e9dcecdf
Jeremy Schlossman <jeremyschlossman@gmail.com>
2023-05-10 20:50:39 -0500

    added prop fix

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,7 +1,7 @@
-
+import { Clock } from "../modules/Clocks"
 // Settings: Takes clocks and updateClock props.
 // Given an array of clocks, displays a list of ClockForm components.
-export function Settings (props: {clocks: Clock, clocks: ClockClockForm:(clocks: Clock)}){
+export function Settings (props: {clocks: Clock[] }){
 
 return ( 
     <div>
