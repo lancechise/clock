f5aefa95359834ceb40b9724082632e1b42d8e78
Kyle Vandenbossche <kylevandenbossche@gmail.com>
2023-05-13 09:56:53 -0400

    Analogclock visible, added digital option checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,42 @@ import logo from './logo.svg';
 import './App.css';
 import { ClockForm } from './components/ClockForm/ClockForm';
 import { Header } from './components/Header/Header';
+import AnalogClock from 'analog-clock-react';
+
 
 function App( ) {
+
+  let options = {
+    width: "200px",
+    border: true,
+    borderColor: "#2e2e2e",
+    baseColor: "#17a2b8",
+    centerColor: "#459cff",
+    centerBorderColor: "#ffffff",
+    handColors: {
+      second: "#d81c7a",
+      minute: "#ffffff",
+      hour: "#ffffff"
+    }
+};
+
+
   return (
     <div className="App">
+     
       <Header />
-      <ClockForm />
+
+      <div className="container">
+
+        <div className="container-settings">
+        <ClockForm />
+        </div>
+      
+        <div className="container-clocks">
+        <AnalogClock {...options}/>
+        </ div>
+
+      </div>
 
     </div>
   );
