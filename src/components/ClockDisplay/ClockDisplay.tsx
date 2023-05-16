01cb1a5d98be9c8d398f2ecfc80aa64d59aea11c
lancechise <lancechise@icloud.com>
2023-05-15 21:19:38 -0400

    type file added

--- src/components/ClockDisplay/ClockDisplay.tsx
+++ src/components/ClockDisplay/ClockDisplay.tsx
@@ -1,27 +1,32 @@
-import { useState, useEffect } from "react"
-import { Clock } from "../../models/Clocks"
+import { useState, useEffect } from "react";
+import { Clock } from "../../models/Clocks";
+const [date, setDate] = useState(new Date());
 
 export function ClockDisplay(props: {}) {
+  function ClockDisplay() {
+    setDate(new Date());
+  }
+  useEffect(() => {
+    const timerId = setInterval(ClockDisplay, 1000);
+    return function cleanup() {
+      clearInterval(timerId);
+    };
+  }, []);
 
+  return;
+  <div>
+    <span>{date.toLocaleTimeString()}</span>
 
-    return(
-        <div>
-            <div>
-                <button>Delete</button>
-            </div>
-
-
-
-        </div>
-    )
+    <div>
+      <button>Delete</button>
+    </div>
+  </div>;
 }
-
-
-
+export default Clock;
 
 // import { useState, useEffect } from 'react';function Clock(){
 //     const [date, setDate] = useState(new Date());
-    
+
 //     function refreshClock() {
 //       setDate(new Date());
 //     }  useEffect(() => {
